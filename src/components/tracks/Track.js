147893dd87f81d316160873dcbe5804b8856ce55
ateet <16ucs050@lnmiit.ac.in>
2019-08-19 03:09:34 +0530

    Day One ended, coding time -- 4hr(slow) reading time -- 3hr

--- src/components/tracks/Track.js
+++ src/components/tracks/Track.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Track(props) {
   const { track } = props;
   return (
@@ -7,6 +7,26 @@ export default function Track(props) {
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
           <h5>{track.artist_name}</h5>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-play" />
+              Track
+            </strong>
+            :{track.track_name}
+            <br />
+            <strong>
+              <i className="fas fa-compact-disc" />
+              Album
+            </strong>
+            :{track.album_name}
+          </p>
+          <Link
+            to={`lyrics/track/${track.track_id}`}
+            className="btn btn-dark btn-block"
+          >
+            <i className="fas fa-chevron-right" />
+            View Lyrics
+          </Link>
         </div>
       </div>
     </div>
