d9baf94e4d208cc759c62ecff8ea9f2e08643634
ateet <16ucs050@lnmiit.ac.in>
2019-08-20 00:51:54 +0530

    track name and lyrics added to card

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
 
 export default class Lyrics extends Component {
   state = {
@@ -30,10 +32,31 @@ track.get?track_id=${this.props.match.params.id}&apikey=${
       .catch(err => console.log(err));
   }
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+    const { track, lyrics } = this.state;
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+        </>
+      );
+    }
   }
 }
